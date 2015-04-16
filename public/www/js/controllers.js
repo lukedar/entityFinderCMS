9f69f0edff89e6de06875f5150d4deafd93a8424
luke.darham <luke.darham@gmail.com>
2015-04-16 13:09:52 +0100

    Refactor map code

--- public/www/js/controllers.js
+++ public/www/js/controllers.js
@@ -17,8 +17,8 @@ angular.module('eventFinder.controllers', ['eventFinder.services'])
 })
 
 .controller('EventCtrl', function($scope, $stateParams, EventsService) {
-  EventsService.query({ eventId: $stateParams.eventId}, function(result) {
-    $scope.event = result[0];
+  EventsService.query({ eventId: $stateParams.eventId}, function(data) {
+    $scope.event = data[0];
   });
 })
 
@@ -53,55 +53,68 @@ angular.module('eventFinder.controllers', ['eventFinder.services'])
   };
 
   // Query Location Service
-  LocationsService.query({locationId: $stateParams.locationId}, function(result) {
+  LocationsService.query({locationId: $stateParams.locationId}, function(data) {
 
-    // Build markers
-    angular.forEach(result, function(item, key) {
-
-      $scope.map.markers[item.node_title] = { 
-        lat : parseFloat(item.location_marker.lat),
-        focus: true, 
-        draggable: false,
-        message:'<h5>' + item.node_title + '</h5>' + '<a class="item-icon-right" href="#/app/locations/' + item.nid + '/detail"><br><i class="icon ion-ios-information-outline"></i></a>',
-        lng : parseFloat(item.location_marker.lng), 
-      };
-
-    });
+    $scope.addMarkers(data);
 
     // When on locations/ID
     if(parseInt($stateParams.locationId, 10)) {
 
       // Set current location.
-      $rootScope.currentLocation = result[0]['location_marker'];
+      $rootScope.currentLocation = data[0]['location_marker'];
 
-        // Add map centering
-        $scope.map.center  = {
-          lat: parseFloat(result[0]['location_marker']['lat']),
-          lng: parseFloat(result[0]['location_marker']['lng']),
-          zoom : 15
-        };
+      console.log(data[0].node_title);
 
+      $scope.centerMap(
+        parseFloat(data[0]['location_marker']['lat']), 
+        parseFloat(data[0]['location_marker']['lng']),
+        data[0].node_title
+        );
     } 
 
   });
+ 
+  // Add marker(s) 
+  $scope.addMarkers = function(data) {
 
+    angular.forEach(data, function(item, key) {
 
-  /**
-  * Get directions based on Geolocation and current map location
-  */
+      $scope.map.markers[item.node_title] = { 
+        lat : parseFloat(item.location_marker.lat),
+        message:'<h5>' + item.node_title + '</h5>' + '<a class="item-icon-right" href="#/app/locations/' + item.nid + '/detail"><br><i class="icon ion-ios-information-outline"></i></a>',
+        lng : parseFloat(item.location_marker.lng), 
+      };
+
+    });
+  }
+
+  // Center Map
+  $scope.centerMap = function(latitude, longitude, markerTitle) {
+
+    if (markerTitle !== null) {
+      // Add Marker
+      $scope.map.markers.now = {
+        lat: latitude,
+        lng: longitude,
+        message: markerTitle,
+        focus: true,
+      };
+    }
+
+    $scope.map.center  = {
+      lat: latitude,
+      lng: longitude,
+      zoom : 15
+    };
+
+  }
+
   $rootScope.getDirections = function(){
 
     // Get Geolocation
     $cordovaGeolocation
       .getCurrentPosition()
       .then(function (position) {
-
-        // Center map on Geolocation point
-        $scope.map.center  = {
-          lat: position.coords.latitude,
-          lng: position.coords.longitude,
-          zoom : 15
-        };
 
         // Add Geolocation Marker
         $scope.map.markers.now = {
@@ -112,15 +125,13 @@ angular.module('eventFinder.controllers', ['eventFinder.services'])
           draggable: false
         };
 
-        // Add Routing 
-        leafletData.getMap().then(function(map) {
-          L.Routing.control({
-            waypoints: [
-                L.latLng(position.coords.latitude, position.coords.longitude),
-                L.latLng($rootScope.currentLocation.lat, $rootScope.currentLocation.lng)
-            ]
-          }).addTo(map);
-        });
+        // Add routing.
+        $scope.addRouting(
+          position.coords.latitude, position.coords.longitude,
+          $rootScope.currentLocation.lat, $rootScope.currentLocation.lng);
+
+        // Center map on user Geolocation.
+        $scope.centerMap(position.coords.latitude, position.coords.longitude, null);
 
       }, function(err) {
         // error
@@ -129,19 +140,31 @@ angular.module('eventFinder.controllers', ['eventFinder.services'])
       });
   };
 
+  // Add Routing 
+  $scope.addRouting = function (fromLat, fromLng, toLat, toLng) {
+    // Get map and add routing.
+    leafletData.getMap().then(function(map) {
+      L.Routing.control({
+        waypoints: [
+            L.latLng(fromLat, fromLng),
+            L.latLng(toLat, toLng)
+        ]
+      }).addTo(map);
+    });
+  }
 
 })
 
 .controller('LocationCtrl', function($scope, $rootScope, $stateParams, LocationsService, EventsByLocationService) {
   
-  LocationsService.query({ locationId: $stateParams.locationId}, function(result) {
-    $scope.location = result[0];
-    $rootScope.currentLocation = result[0]['location_marker'];
+  LocationsService.query({ locationId: $stateParams.locationId}, function(data) {
+    $scope.location = data[0];
+    $rootScope.currentLocation = data[0]['location_marker'];
   });
 
 
-  EventsByLocationService.query({ locationId: $stateParams.locationId}, function(result) {
-    $scope.events = result;
+  EventsByLocationService.query({ locationId: $stateParams.locationId}, function(data) {
+    $scope.events = data;
   });
 
 });
